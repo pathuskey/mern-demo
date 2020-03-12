5d2b812ba901a91afc08428206af78dc865fea8a
HUSKEYP <patrick.huskey@tyson.com>
2020-03-12 15:42:09 -0500

    Setup deploy to google app engine

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://server.meandemo.com:8080/api"
+  baseURL: "http://server.meandemo.com:8000/api"
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
